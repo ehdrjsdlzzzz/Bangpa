f63240a758df4f5ced478e4d4bded188a1cad2a8
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-12-03 15:04:47 +0900

    Files Directory chanege

--- Bangpa/Cells/SearchStudyGroupViewController/RecruitPostTableViewCell.swift
+++ Bangpa/Cells/SearchStudyGroupViewController/RecruitPostTableViewCell.swift
@@ -2,8 +2,8 @@
 //  RecruitPostTableViewCell.swift
 //  Bangpa
 //
-//  Created by RAK on 2018. 8. 7..
-//  Copyright © 2018년 이동건. All rights reserved.
+//  Created by 이동건 on 03/12/2018.
+//  Copyright © 2018 이동건. All rights reserved.
 //
 
 import UIKit
@@ -14,17 +14,41 @@ class RecruitPostTableViewCell: UITableViewCell {
     @IBOutlet weak var recruitNumberLabel: UILabel!
     @IBOutlet weak var postWriterLabel: UILabel!
     @IBOutlet weak var elapsedTimeLabel: UILabel!
+    @IBOutlet weak var bookMarkButton: UIButton!
+    
+    
+    var isCheckedBookMark = false
+    @IBAction func bookMarkButtonTapped(_ sender: Any) {
+        isCheckedBookMark = !isCheckedBookMark
+        
+        setUpViews()
+    }
+    
+    private func setUpViews() {
+        if isCheckedBookMark {
+            bookMarkButton.setImage(#imageLiteral(resourceName: "SelectedBookmark"), for: .normal)
+        } else {
+            bookMarkButton.setImage(#imageLiteral(resourceName: "NoneSelectedBookmark"), for: .normal)
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        contentView.frame = UIEdgeInsetsInsetRect(contentView.frame, UIEdgeInsetsMake(0, 16, 0, 16))
+        setUpViews()
+        //        contentView.frame = UIEdgeInsetsInsetRect(contentView.frame, UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
         // Initialization code
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        postTitleLabel.text = nil
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
---
+++ Bangpa/Cells/SearchStudyGroupViewController/RegionCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RegionCollectionViewCell.swift
+//  Bangpa
+//
+//  Created by 이동건 on 03/12/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class RegionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var regionLabel: UILabel!
+    
+}
